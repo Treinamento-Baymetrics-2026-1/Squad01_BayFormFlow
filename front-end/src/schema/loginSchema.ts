eb9383aabb341766f6ec4a1955fceea84a60d4a3
Kayky de Souza <desouzakayky05@gmail.com>
2026-06-18 11:57:03 -0300

    feat: add login modal with zod verification

---
+++ front-end/src/schema/loginSchema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Insira um e-mail é válido" })
+    .email({ message: "Digite um e-mail válido" }),
+  password: z
+    .string()
+    .min(1, { message: "A senha é obrigatória" })
+    .min(6, { message: "A senha deve ter pelo menos 6 caracteres." }),
+});
