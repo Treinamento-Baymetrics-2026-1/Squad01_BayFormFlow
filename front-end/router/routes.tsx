232dbb89c444c82e4d461baae598a5951b33f34f
Kayky de Souza <desouzakayky05@gmail.com>
2026-06-23 11:36:35 -0300

    feat: add route to survey page

--- front-end/router/routes.tsx
+++ front-end/router/routes.tsx
@@ -1,3 +1,4 @@
+import { SurveyPage } from "@/components/AdmProfile/Survey";
 import { AuthPage } from "@/components/Auth/Auth";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,6 +8,7 @@ export const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/pesquisas" element={<SurveyPage />} />
       </Routes>
     </BrowserRouter>
   );
