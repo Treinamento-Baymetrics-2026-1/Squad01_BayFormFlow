c56462d87c3f5716e47d49c2aaae84fa62589ed1
Kayky de Souza <desouzakayky05@gmail.com>
2026-07-07 15:29:04 -0300

    feat: create form NewSurvey component

---
+++ front-end/src/components/NewSurvey/NewSurveyForm.tsx
@@ -0,0 +1,14 @@
+export const NewSurveyForm = () => {
+  return (
+    <div className="">
+      <div className="flex justify-between items-center w-full">
+        <h3 className="font-bold text-2xl text-blue-primary">Pesquisas</h3>
+      </div>
+      <div>
+        <p className="font-normal text-base leading-6 tracking-normal text-gray-placeholder">
+          Pesquisas às quais você tem acesso
+        </p>
+      </div>
+    </div>
+  );
+};
