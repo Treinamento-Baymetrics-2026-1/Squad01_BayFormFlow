cb2011ef656f2420d25fabe71a8d9729f7b86e72
0o0Musiquinha <0o0musiquinha@gmail.com>
2026-07-10 17:22:48 -0300

    feat: added foreign key constraints

--- back-end/migrations/V011__create_t_participants.sql
+++ back-end/migrations/V011__create_t_participants.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS requesters.t_participants(
     participant_name    VARCHAR(120)    NOT NULL,
     email               VARCHAR(254)    NULL,
     rm                  VARCHAR(30)     NULL,
-    is_deleted          BOOLEAN         NOT NULL,
+    is_deleted          BOOLEAN         NOT NULL DEFAULT FALSE,
     deleted_at          TIMESTAMPTZ     NULL,
     companie_id         SMALLINT        NOT NULL,
     deleted_by          UUID            NULL,
@@ -19,5 +19,17 @@ CREATE TABLE IF NOT EXISTS requesters.t_participants(
     CONSTRAINT requesters_t_participants_ck_email
         CHECK(
             email ~ '^[^.]{1}(?!.*\.\.)[A-Za-z0-9._%+-]{0,62}[^.]{1}@(?!.*\.\.)[^._%+-]{1}[A-Za-z0-9.-]{0,190}\..*[^._%+-]{1}$'
-        )
+        ),
+    CONSTRAINT requesters_t_participants_ck_is_deleted
+        CHECK(
+            (is_deleted = FALSE AND deleted_at IS NULL)
+            OR
+            (is_deleted = TRUE AND deleted_at IS NOT NULL)
+        ),
+        
+    --FKs
+    CONSTRAINT requesters_t_participants_fk_companie_id
+        FOREIGN KEY (companie_id) REFERENCES requesters.t_companies (id),
+    CONSTRAINT requesters_t_participants_fk_deleted_by
+        FOREIGN KEY (deleted_by) REFERENCES logins.t_users (id)
 );
