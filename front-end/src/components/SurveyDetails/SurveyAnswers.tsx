27b7f1893198aee2a8d7763875595169497d9216
Kayky de Souza <desouzakayky05@gmail.com>
2026-07-14 18:05:13 -0300

    fix: fix lint errors and warnings

--- front-end/src/components/SurveyDetails/SurveyAnswers.tsx
+++ front-end/src/components/SurveyDetails/SurveyAnswers.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 export const SurveyAnswers = () => {
   const answers = [
--- front-end/src/components/SurveyDetails/SurveyDetailsMain.tsx
+++ front-end/src/components/SurveyDetails/SurveyDetailsMain.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Calendar, Users as UsersIcon, Plus, Form } from "lucide-react";
 import { SurveyAddedForms } from "./SurveyAddedForms";
 import { SurveyParticipants } from "./SurveyParticipants";
